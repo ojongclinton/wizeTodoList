eba655b1c32ada8d1e822131643958e7520b0c54
ojong <82442816+ojongclinton@users.noreply.github.com>
2024-05-01 22:43:29 +0100

    initial users and todos

---
+++ src/utils/mocks/mockDataIni.ts
@@ -0,0 +1,72 @@
+import { v4 as uuidv4 } from "uuid";
+import { Labels, Priority, Todo } from "@/types/Todo";
+
+export let initialUsers: Assignee[] = [
+  {
+    id: uuidv4(),
+    name: "John Doe",
+    email: "jhonDoe@gmail.com",
+    phone: "655435890",
+  },
+  {
+    id: uuidv4(),
+    name: "Diamond Jackson",
+    email: "diamondJackson@gmail.com",
+    phone: "688434570",
+  },
+  {
+    id: uuidv4(),
+    name: "Ronaldo Decaprio",
+    email: "ronaldoDecaprio@gmail.com",
+    phone: "655435890",
+  },
+  {
+    id: uuidv4(),
+    name: "Messi muhamad",
+    email: "messimuhammed@gmail.com",
+    phone: "655435890",
+  },
+];
+
+export let initialTodos: Todo[] = [
+  {
+    id: uuidv4(),
+    title: "Buy milk",
+    assignee: null,
+    startDate: new Date(),
+    endDate: new Date(),
+    description: "Buy milk from the store",
+    priority: Priority.HIGH,
+    labels: [Labels.BACKLOG],
+  },
+  {
+    id: uuidv4(),
+    title: "Buy eggs",
+    assignee: null,
+    startDate: new Date(),
+    endDate: new Date(),
+    description: "Buy eggs from the store",
+    priority: Priority.HIGH,
+    labels: [Labels.BACKLOG],
+  },
+  {
+    id: uuidv4(),
+    title: "Buy bread",
+    assignee: null,
+    startDate: new Date(),
+    endDate: new Date(),
+    description: "Buy bread from the store",
+    priority: Priority.MEDIUM,
+    labels: [Labels.BACKLOG],
+  },
+  {
+    id: uuidv4(),
+    title: "Buy butter",
+    assignee: null,
+    startDate: new Date(),
+    endDate: new Date(),
+    description: "Buy butter from the store",
+    priority: Priority.LOW,
+    labels: [Labels.BACKLOG],
+  },
+];
